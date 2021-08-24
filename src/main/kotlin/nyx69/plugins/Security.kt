e8b4699005366c5d8ed3cd2d2efe29d8ad443729
pentexnyx <loool1912@web.de>
2021-08-24 21:38:28 +0200

    heroku test

--- src/main/kotlin/nyx69/plugins/Security.kt
+++ src/main/kotlin/nyx69/plugins/Security.kt
@@ -1,23 +1,35 @@
 package nyx69.plugins
 
+import com.auth0.jwk.JwkProviderBuilder
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.application.*
 import io.ktor.auth.*
 import io.ktor.auth.jwt.*
+import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
 import io.ktor.sessions.*
+import java.util.concurrent.TimeUnit
 import kotlin.collections.set
 
 fun Application.configureSecurity() {
 
     authentication {
-        jwt {
+        jwt("auth-jwt") {
 
 
-            val jwtAudience = "jwt.audience"
+            val jwtAudience = "http://0.0.0.0:8080/hello"
             realm = "jwt.realm"
+   /*       val  issuer = "http://0.0.0.0:8080/"
+        val    audience = "http://0.0.0.0:8080/hello"
+
+
+            val jwkProvider = JwkProviderBuilder(issuer)
+                .cached(10, 24, TimeUnit.HOURS)
+                .rateLimited(10, 1, TimeUnit.MINUTES)
+                .build() */
+
             verifier(
                 JWT
                     .require(Algorithm.HMAC256("secret"))
@@ -26,8 +38,19 @@ fun Application.configureSecurity() {
                     .build()
             )
             validate { credential ->
-                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+             //   if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+
+                    if (credential.payload.getClaim("username").asString() != "") {
+                        JWTPrincipal(credential.payload)
+                    } else {
+                        null
+                    }
+
             }
+
+
+
+
 
 
         }
@@ -47,6 +70,32 @@ fun Application.configureSecurity() {
             val session = call.sessions.get<MySession>() ?: MySession()
             call.sessions.set(session.copy(count = session.count + 1))
             call.respondText("Counter is ${session.count}. Refresh to increment.")
+        }
+
+    /*    post("/login") {
+            val user = call.receive<User>()
+
+            val publicKey = jwkProvider.get("6f8856ed-9189-488f-9011-0ff4b6c08edc").publicKey
+            val keySpecPKCS8 = PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyString))
+            val privateKey = KeyFactory.getInstance("RSA").generatePrivate(keySpecPKCS8)
+            val token = JWT.create()
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .withClaim("username", user.username)
+                .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                .sign(Algorithm.RSA256(publicKey as RSAPublicKey, privateKey as RSAPrivateKey))
+            call.respond(hashMapOf("token" to token))
+        }
+
+     */
+
+        authenticate("auth-jwt") {
+            get("/hello") {
+                val principal = call.principal<JWTPrincipal>()
+                val username = principal!!.payload.getClaim("username").asString()
+                val expiresAt = principal.expiresAt?.time?.minus(System.currentTimeMillis())
+                call.respondText("Hello, $username! Token is expired at $expiresAt ms.")
+            }
         }
 
 
