16382f1f76264e4edf0b80fa83a75b44e5d6189f
pentexnyx <loool1912@web.de>
2021-08-24 21:54:27 +0200

    heroku test

--- src/main/kotlin/nyx69/plugins/Security.kt
+++ src/main/kotlin/nyx69/plugins/Security.kt
@@ -6,16 +6,22 @@ import com.auth0.jwt.algorithms.Algorithm
 import io.ktor.application.*
 import io.ktor.auth.*
 import io.ktor.auth.jwt.*
+import io.ktor.http.content.*
 import io.ktor.request.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.sessions.*
+import kotlinx.serialization.Serializable
+import java.io.File
+import java.security.KeyFactory
+import java.security.interfaces.RSAPrivateKey
+import java.security.interfaces.RSAPublicKey
+import java.security.spec.PKCS8EncodedKeySpec
+import java.util.*
 import java.util.concurrent.TimeUnit
-import kotlin.collections.set
 
 fun Application.configureSecurity() {
 
-    authentication {
+  /*  authentication {
         jwt("auth-jwt") {
 
 
@@ -72,7 +78,7 @@ fun Application.configureSecurity() {
             call.respondText("Counter is ${session.count}. Refresh to increment.")
         }
 
-    /*    post("/login") {
+        post("/login") {
             val user = call.receive<User>()
 
             val publicKey = jwkProvider.get("6f8856ed-9189-488f-9011-0ff4b6c08edc").publicKey
@@ -87,7 +93,7 @@ fun Application.configureSecurity() {
             call.respond(hashMapOf("token" to token))
         }
 
-     */
+
 
         authenticate("auth-jwt") {
             get("/hello") {
@@ -100,4 +106,74 @@ fun Application.configureSecurity() {
 
 
     }
+
+   */
+
+
+
+
+
+
+
+
+    val privateKeyString = environment.config.property("jwt.privateKey").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    val myRealm = environment.config.property("jwt.realm").getString()
+    val jwkProvider = JwkProviderBuilder(issuer)
+        .cached(10, 24, TimeUnit.HOURS)
+        .rateLimited(10, 1, TimeUnit.MINUTES)
+        .build()
+    install(Authentication) {
+        jwt("auth-jwt") {
+            realm = myRealm
+            verifier(jwkProvider, issuer) {
+                acceptLeeway(3)
+            }
+            validate { credential ->
+                if (credential.payload.getClaim("username").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+        }
+    }
+    routing {
+        post("/login") {
+            val user = call.receive<User>()
+            // Check username and password
+            // ...
+            val publicKey = jwkProvider.get("6f8856ed-9189-488f-9011-0ff4b6c08edc").publicKey
+            val keySpecPKCS8 = PKCS8EncodedKeySpec(Base64.getDecoder().decode(privateKeyString))
+            val privateKey = KeyFactory.getInstance("RSA").generatePrivate(keySpecPKCS8)
+            val token = JWT.create()
+                .withAudience(audience)
+                .withIssuer(issuer)
+                .withClaim("username", user.username)
+                .withExpiresAt(Date(System.currentTimeMillis() + 60000))
+                .sign(Algorithm.RSA256(publicKey as RSAPublicKey, privateKey as RSAPrivateKey))
+            call.respond(hashMapOf("token" to token))
+        }
+
+        authenticate("auth-jwt") {
+            get("/hello") {
+                val principal = call.principal<JWTPrincipal>()
+                val username = principal!!.payload.getClaim("username").asString()
+                val expiresAt = principal.expiresAt?.time?.minus(System.currentTimeMillis())
+                call.respondText("Hello, $username! Token is expired at $expiresAt ms.")
+            }
+        }
+        static(".well-known") {
+            staticRootFolder = File("certs")
+            file("jwks.json")
+        }
+    }
+
+
+
+
 }
+
+@Serializable
+data class User(val username: String, val password: String)
