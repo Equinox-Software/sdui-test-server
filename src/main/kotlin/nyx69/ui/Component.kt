53b9596c7fa84f2b79f0a193ae0cd580208d36e0
pentexnyx <loool1912@web.de>
2021-09-05 21:58:09 +0200

    fiddling

--- src/main/kotlin/nyx69/ui/Component.kt
+++ src/main/kotlin/nyx69/ui/Component.kt
@@ -1,8 +1,10 @@
 package nyx69.ui
 
+import kotlinx.serialization.Contextual
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonPrimitive
 import kotlinx.serialization.json.encodeToJsonElement
 import nyx69.ui.ComponentType.*
 
@@ -10,16 +12,16 @@ import nyx69.ui.ComponentType.*
 data class Component(
     val id: String,
     val type: ComponentType,
-    val data: JsonElement? = null,
+    val data: @Contextual Any? = null,
     val children: List<Component>? = null
 )
 
 @Suppress("FunctionName")
 object Widget {
-    fun CText(id: String, text: String) = Component(id, TEXT, Json.encodeToJsonElement(text))
-    fun CEditText(id: String, text: String) = Component(id, TEXT, Json.encodeToJsonElement(text))
-    fun CImage(id: String, url: String) = Component(id, IMAGE, Json.encodeToJsonElement(url))
-    fun CButton(id: String, text: String) = Component(id, BUTTON, Json.encodeToJsonElement(text))
+    fun CText(id: String, text: String) = Component(id, TEXT,text)
+    fun CEditText(id: String, text: String) = Component(id, TEXT, text)
+    fun CImage(id: String, url: String) = Component(id, IMAGE, url)
+    fun CButton(id: String, text: String) = Component(id, BUTTON, text)
 }
 
 @Suppress("FunctionName")
