3e88ffdf3d1cfc301b96e82f64ccfd6da15c73f6
pentexnyx <loool1912@web.de>
2021-09-05 21:27:48 +0200

    fiddling

--- src/main/kotlin/nyx69/Application.kt
+++ src/main/kotlin/nyx69/Application.kt
@@ -13,7 +13,7 @@ import nyx69.plugins.configureSecurity
 import org.slf4j.event.Level
 
 fun main() {
-    embeddedServer(CIO, 9090/*System.getenv("PORT").toInt()*/) {
+    embeddedServer(CIO, System.getenv("PORT").toInt()) {
         install(ContentNegotiation) {
             json(Json {
                 //    prettyPrint = true
