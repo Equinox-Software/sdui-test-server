7f2c6154942e6c28f14e4660bfdbdf0d31f27a1b
pentexnyx <loool1912@web.de>
2021-09-05 21:49:55 +0200

    fiddling

--- src/main/kotlin/nyx69/ui/Component.kt
+++ src/main/kotlin/nyx69/ui/Component.kt
@@ -1,29 +1,32 @@
 package nyx69.ui
 
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.encodeToJsonElement
+import nyx69.ui.ComponentType.*
 
 @Serializable
 data class Component(
     val id: String,
     val type: ComponentType,
-    val data: Map<String, JsonPrimitive>? = null,
+    val data: JsonElement? = null,
     val children: List<Component>? = null
 )
 
 @Suppress("FunctionName")
 object Widget {
-    fun CText(id: String, text: String) = Component(id, ComponentType.TEXT, text)
-    fun CEditText(id: String, text: String) = Component(id, ComponentType.TEXT, text)
-    fun CImage(id: String, url: String) = Component(id, ComponentType.IMAGE, url)
-    fun CButton(id: String, text: String) = Component(id, ComponentType.BUTTON, text)
+    fun CText(id: String, text: String) = Component(id, TEXT, Json.encodeToJsonElement(text))
+    fun CEditText(id: String, text: String) = Component(id, TEXT, Json.encodeToJsonElement(text))
+    fun CImage(id: String, url: String) = Component(id, IMAGE, Json.encodeToJsonElement(url))
+    fun CButton(id: String, text: String) = Component(id, BUTTON, Json.encodeToJsonElement(text))
 }
 
 @Suppress("FunctionName")
 object Layout {
-    fun CColumn(id: String, children: List<Component>) = Component(id, ComponentType.VERTICAL, children = children)
+    fun CColumn(id: String, children: List<Component>) = Component(id, VERTICAL, children = children)
     fun CLazyColumn(id: String, children: List<Component>) =
-        Component(id, ComponentType.SCROLL_VERTICAL, children = children)
+        Component(id, SCROLL_VERTICAL, children = children)
 
-    fun CBox(id: String, children: List<Component>) = Component(id, ComponentType.BOX, children = children)
+    fun CBox(id: String, children: List<Component>) = Component(id, BOX, children = children)
 }
