e11c7720c4cba352e48d9a011a5b76eceda4a089
Felix Huber <felix.huber@ametras.com>
2021-09-06 17:14:13 +0200

    styles test

--- src/main/kotlin/nyx69/ui/ComponentStyle.kt
+++ src/main/kotlin/nyx69/ui/ComponentStyle.kt
@@ -5,6 +5,9 @@ import kotlinx.serialization.Serializable
 @Serializable
 sealed class ComponentStyle(val type: ComponentStyleType) {
     data class PaddingAll(val all: Int) : ComponentStyle.Padding(all, all, all, all)
-    open class Padding(top: Int, start: Int, end: Int, bottom: Int) : ComponentStyle(ComponentStyleType.PADDING)
+    data class PaddingDirectional(val vertical: Int, val horizontal: Int) :
+        ComponentStyle.Padding(vertical, horizontal, vertical, horizontal)
+
+    open class Padding(start: Int, top: Int, end: Int, bottom: Int) : ComponentStyle(ComponentStyleType.PADDING)
     data class Color(val color: Long) : ComponentStyle(ComponentStyleType.COLOR)
 }
