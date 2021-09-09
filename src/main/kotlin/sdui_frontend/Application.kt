fa28ced77b4ac7c1ef6cf38035ee263557146eff
Felix Huber <felix.huber@ametras.com>
2021-09-09 16:25:54 +0200

    ktor modules suck

--- src/main/kotlin/sdui_frontend/Application.kt
+++ src/main/kotlin/sdui_frontend/Application.kt
@@ -1,39 +1,36 @@
 package sdui_frontend
 
-import io.ktor.application.Application
+import io.ktor.application.*
 import io.ktor.client.features.HttpCallValidator.Companion.install
 import io.ktor.features.CallLogging
 import io.ktor.features.ContentNegotiation
-import io.ktor.features.ContentNegotiation.Feature.install
+import io.ktor.response.*
+import io.ktor.routing.*
 import io.ktor.server.cio.CIO
-import io.ktor.server.engine.ApplicationEngineFactory
-import io.ktor.server.engine.embeddedServer
+import io.ktor.server.engine.*
 import kotlinx.serialization.json.Json
 import org.slf4j.event.Level
 
 
-fun main() {
-    embeddedServer(CIO as ApplicationEngineFactory<*, *>, System.getenv("PORT").toInt()) {
-     install(ContentNegotiation) {
-            json(Json {
-                prettyPrint = true
-                //     isLenient = true
-                ignoreUnknownKeys = true
-            })
-        }
-        /*
-              install(CallLogging) {
-                  level = Level.INFO
-                  filter { call -> call.request.path().startsWith("/") }
-              }
+fun main() { embeddedServer(CIO as ApplicationEngineFactory<*, *>, System.getenv("PORT").toInt(), module = Application::module).start(wait = true)}
 
-              */
+fun Application.module(){
+    install(ContentNegotiation) {
+        json(Json {
+            prettyPrint = true
+            //     isLenient = true
+            ignoreUnknownKeys = true
+        })
+    }
 
-      //  configureSecurity()
-
-       // configureRouting()
-
-    }.start(wait = true)
+          install(CallLogging) {
+              level = Level.INFO
+              filter { call -> call.request.path().startsWith("/") }
+          }
 
 
+
+    //  configureSecurity()
+
+    // configureRouting()
 }
